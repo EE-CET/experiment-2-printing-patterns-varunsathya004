47d89a3332846f5c78501e0527d02f62b46246ab
Varun Sathya <tve23ee131@cet.ac.in>
2026-04-10 13:58:38 +0530

    final commit

--- src/PatternPrinting.java
+++ src/PatternPrinting.java
@@ -1,5 +1,22 @@
 import java.util.Scanner;
 
 public class PatternPrinting {
-    
+    public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter n: ");
+        if (sc.hasNextInt()) {
+            int n = sc.nextInt();
+            for (int i = 0; i < n; i++) {
+                if (i < n - 1) {
+                    System.out.print("* ");
+                } else {
+                    System.out.println("*");
+                }
+            }
+            for (int i = 0; i < n; i++) {
+                System.out.println("*");
+            }
+        }
+        sc.close();
+    }
 }
